7173ce72bf7ad8aff7aca45dbb95f46e95955180
Dayene <40864976+Dayene@users.noreply.github.com>
2018-08-18 23:31:17 -0300

    completed the module

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+
+
+
 const fs = require('fs');
 const path = require('path');
 
@@ -7,6 +10,10 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded( { extended: true }));
+
 
 const accountData = fs.readFileSync(
     path.join(__dirname, 'json','accounts.json'), 'utf8'
@@ -20,8 +27,6 @@ const users = JSON.parse(userData);
 
 
  
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get ('/', (req, res ) => { res.render('index', {title: 'Account Summary', accounts })
 });
 
@@ -32,6 +37,37 @@ app.get ('/checking', (req, res ) => { res.render('account', {account:  accounts
 });
 
 app.get ('/credit', (req, res ) => { res.render('account', {account:  accounts.credit })
+});
+
+app.get ('/transfer', (req, res ) =>  res.render('transfer'));
+app.post ('/transfer', (req, res ) =>  {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: 'Transfer completed'});
+});
+
+app.get ('/payment', (req, res ) =>  res.render('payment',{account: accounts.credit }));
+app.post('/payment', (req, res ) =>  {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount,10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accountsJSON'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit });
+});
+
+
+
+app.get ('/transfer', (req, res ) =>  res.render('transfer'));
+app.post ('/transfer', (req, res ) =>  {
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: 'Transfer completed'});
 });
 
 app.get ('/profile', (req, res ) => { res.render('profile', {user:  users[0] })
